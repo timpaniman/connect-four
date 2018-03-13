cfcd66db272737448a7e21caafa709050f4f940e
Darius Bacon <darius@wry.me>
2018-03-12 18:43:44 -0700

    add won() (incompletely tested)

--- connect_n.js
+++ connect_n.js
@@ -1,5 +1,9 @@
-// Connect-5 board
+// Connect-N board
 
+// How many pieces in a row are needed to win.
+const nstreak = 3;   // I think we want 5? But this is easier to test.
+
+// The size of the board
 const nrows = 6;
 const ncols = 7;
 
@@ -11,9 +15,9 @@ function makeBoard() {
     return cols;
 }
 
-// Mutate the board: drop the piece into the column.
-function drop(board, col, piece) {
-    board[col].push(piece);
+// Mutate the board: drop the piece into column c.
+function drop(board, c, piece) {
+    board[c].push(piece);
 }
 
 // The piece at (r,c): 1, 2, or 0 (meaning empty).
@@ -37,17 +41,56 @@ function show(board) {
     return rows.join('\n');
 }
 
+// Precondition: the last move was to column c.
+// Did that move win the game?
+function won(board, c) {
+    const col = board[c];
+    const r = col.length - 1;
+    
+    return (   streak(board, r, c, 1, 0)    // vertical
+            || streak(board, r, c, 0, 1)    // horizontal
+            || streak(board, r, c, 1, 1)    // rising diagonal
+            || streak(board, r, c, 1, -1)); // falling diagonal
+}
+
+// Is there a streak of at least nstreak pieces of the same type going
+// through (r0,c0) in the directior (dr,dc)?
+function streak(board, r0, c0, dr, dc) {
+    const piece = at(board, r0, c0);
+    let n = 1;
+    for (let r = r0+dr, c = c0+dc; piece === at(board, r, c); r += dr, c += dc) {
+        n += 1;
+    }
+    for (let r = r0-dr, c = c0-dc; piece === at(board, r, c); r -= dr, c -= dc) {
+        n += 1;
+    }
+    return nstreak <= n;
+}
+
 
 // Smoke test
 
 function testme() {
     const b = makeBoard();
+
     console.log(show(b));
     console.log();
+
     drop(b, 0, 1);
     drop(b, 0, 2);
     drop(b, 2, 1);
     console.log(show(b));
+    console.log(won(b, 2));
+    console.log();
+
+    drop(b, 0, 2);
+    console.log(show(b));
+    console.log(won(b, 0));
+    console.log();
+
+    drop(b, 1, 1);
+    console.log(show(b));
+    console.log(won(b, 1));
     console.log();
 }
 
